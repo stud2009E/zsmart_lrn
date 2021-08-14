b1921094aef39f73b7d47b8796b468d1322f6207
unknown <stud2009@yandex.ru>
2021-08-14 16:29:47 +0300

    [FIX] add new form with responsive layout

--- webapp/controller/SmartForm.controller.js
+++ webapp/controller/SmartForm.controller.js
@@ -9,7 +9,9 @@ sap.ui.define([
 
         onInit : function () {
 
-            this.getRouter().getRoute("smartForm").attachMatched(this._onRouteMatch, this);
+            this.getRouter()
+                .getRoute("smartForm")
+                .attachMatched(this._onRouteMatch, this);
         },
 
         /**
@@ -17,15 +19,25 @@ sap.ui.define([
          * @param {sap.ui.base.Event} oEvent 
          */
         _onRouteMatch: function(oEvent){
-            var oSmartForm = this.byId("smartForm");
+            var oSmartForm1 = this.byId("smartForm1");
+            var oSmartForm2 = this.byId("smartForm2");
             var oModel = this.getModel();
-            var sPath = oModel.createKey("/HeaderSet", {
+            var sPath1 = oModel.createKey("/HeaderSet", {
                 Guid: "ecd40596-33eb-4192-b6f1-8a3ce393369a"
             });
 
+            var sPath2 = sPath1;
+            sPath2 = oModel.createKey("/HeaderSet", {
+                Guid: "99739da1-0057-420c-83c7-b16abaf6bd89"
+            });
+
             oModel.metadataLoaded().then(function(){
-                oSmartForm.bindElement({
-                    path: sPath
+                oSmartForm1.bindElement({
+                    path: sPath1
+                });
+
+                oSmartForm2.bindElement({
+                    path: sPath2
                 });
             });
         }
